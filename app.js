5c18738f1e7542bcc6cab8df7b3b68dc4148368f
AmukthaMalyadaGaje <amukthagaje@gmail.com>
2025-01-19 20:12:18 +0530

    Updated code!!

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+const cors = require('cors');
 const express = require('express');
+const app = express();
+
+
+// Enable CORS for requests from http://localhost:5173 (or all origins)
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from the React app
+    methods: ['GET', 'POST'], // Allow specific methods if needed
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers if needed
+}));
+
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const transactionRoutes = require('./routes/transactionRoutes');
 
-const app = express();
 app.use(bodyParser.json());
 app.use('/api', transactionRoutes);
 
